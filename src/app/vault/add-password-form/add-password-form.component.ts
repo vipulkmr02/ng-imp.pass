9d696f9bc9a454220e27c4954cfb3ad07fc74b54
Vipul Kumar Gupta <vipulkmr02@gmail.com>
2025-08-18 07:41:45 +0530

    Blurred
    Unable to navigate and create this, because I am exhausted in my UiPath
    POCs, apart from that I am overthinking about my character, I got
    serious doubts about myself.
    
    Bugs:
      - Navbar won't show logout button just after login.
      - Vault's page not opening after clicking 'Enter Vault' button a
        thousand times.

--- src/app/vault/add-password-form/add-password-form.component.ts
+++ src/app/vault/add-password-form/add-password-form.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-add-password-form',
   standalone: true,
-  imports: [MatFormFieldModule, MatDialogModule, MatButtonModule, ReactiveFormsModule],
+  imports: [
+    MatFormFieldModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './add-password-form.component.html',
   styleUrl: './add-password-form.component.css'
 })
 export class AddPasswordFormComponent {
+  passwordVisibility = false;
+  form = new FormGroup({
+    pID: new FormControl("", [Validators.required]),
+    password: new FormControl("", [Validators.required])
+  })
 
 }
